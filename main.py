791f2c18b861ff2c887efe1e7e8edf32eae15542
fazeel123 <thinksimple123@hotmail.com>
2020-01-09 22:40:48 +0800

    Format Changes

--- main.py
+++ main.py
@@ -59,37 +59,43 @@ class Blockchain:
             block_index += 1
         return True
 
-app = Flask(__name__)
-blockchain = Blockchain()
+    # Initialize Flask App
+    app = Flask(__name__)
 
-@app.route('/mine_block', methods = ['GET'])
-def mine_block():
-    previous_block = blockchain.get_previous_block()
-    previous_proof = previous_block['proof']
-    proof = blockchain.proof_of_work(previous_proof)
-    previous_hash = blockchain.hash(previous_block)
-    block = blockchain.create_block(proof, previous_hash)
-    response = {'message': 'Congratulations!',
-                'index': block['index'],
-                'timestamp': block['timestamp'],
-                'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
-    return jsonify(response), 200
+    # Class Blockchain Instance
+    blockchain = Blockchain()
 
-@app.route('/get_chain', methods = ['GET'])
-def get_chain():
-    response = {'chain': blockchain.chain,
-                'length': len(blockchain.chain)}
-    return jsonify(response), 200 
+    # Route to mining blocks
+    @app.route('/mine_block', methods = ['GET'])
+    def mine_block():
+        previous_block = blockchain.get_previous_block()
+        previous_proof = previous_block['proof']
+        proof = blockchain.proof_of_work(previous_proof)
+        previous_hash = blockchain.hash(previous_block)
+        block = blockchain.create_block(proof, previous_hash)
+        response = {'message': 'Congratulations!',
+                    'index': block['index'],
+                    'timestamp': block['timestamp'],
+                    'proof': block['proof'],
+                    'previous_hash': block['previous_hash']}
+        return jsonify(response), 200
 
-# Checking if the Blockchain is valid
-@app.route('/is_valid', methods = ['GET'])
-def is_valid():
-    is_valid = blockchain.is_chain_valid(blockchain.chain)
-    if is_valid:
-        response = {'message': 'All good. The Blockchain is valid.'}
-    else:
-        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
-    return jsonify(response), 200           
+    # Route for getting results
+    @app.route('/get_chain', methods = ['GET'])
+    def get_chain():
+        response = {'chain': blockchain.chain,
+                    'length': len(blockchain.chain)}
+        return jsonify(response), 200 
 
-app.run(host = '0.0.0.0', port = 5000)
+    # Checking if the Blockchain is valid
+    @app.route('/is_valid', methods = ['GET'])
+    def is_valid():
+        is_valid = blockchain.is_chain_valid(blockchain.chain)
+        if is_valid:
+            response = {'message': 'All good. The Blockchain is valid.'}
+        else:
+            response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+        return jsonify(response), 200           
+
+    # Run App
+    app.run(host = '0.0.0.0', port = 5000)
